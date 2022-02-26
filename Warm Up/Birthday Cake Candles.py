3f19ba92144d11001043a67b0cde1f76d5760a79
liuyibo-leo <63709525+liuyibo-leo@users.noreply.github.com>
2022-02-26 14:45:37 +0800

    Update Birthday Cake Candles.py
    if I put the constraints in the code, the system did not consider the code is the correct answer. therefore I removed the constraints (the "if/conditions code").

--- Warm Up/Birthday Cake Candles.py
+++ Warm Up/Birthday Cake Candles.py
@@ -15,10 +15,12 @@ import sys
 
 def birthdayCakeCandles(candles):
     # Write your code here
-    if 1<= candles_count <= 10^5 and [1<= i <= 10^7 for i in candles]:
-        Max = max(candles)
-        counts = candles.count(Max)
-        return counts
+    counts = 0
+    Max = max(candles)
+    for i in range(len(candles)):
+        if (candles[i] == Max):
+            counts = counts + 1
+    return counts
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -32,6 +34,7 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
 
     
 ############
@@ -47,5 +50,46 @@ def birthdayCakeCandles(candles):
         Max = max(candles)
         counts = candles.count(Max)
         return counts
+
+print(birthdayCakeCandles(candles))
+
+#method 2
+def birthdayCakeCandles(candles):
+    # Write your code here
+    counts = 0
+    Max = max(candles)
+    if 1<= candles_count <= 10^5 and [1<= i <= 10^7 for i in candles]:
+        for i in candles:
+            if i == Max:
+                counts = counts + 1
+        return counts
+    
+ #method 3
+def birthdayCakeCandles(candles):
+    # Write your code here
+    counts = 0
+    Max = max(candles)
+    if 1<= candles_count <= 10^5 and [1<= i <= 10^7 for i in candles]:
+        for i in range(len(candles)):
+            if candles[i] == Max:
+                counts = counts + 1
+        return counts
+
+
+#final answer:
+candles_count = 4
+candles = [3, 2, 1, 3]
+
+
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    counts = 0
+    Max = max(candles)
+    for i in range(len(candles)):
+        if (candles[i] == Max):
+            counts = counts + 1
+    return counts
+        
 
 print(birthdayCakeCandles(candles))
